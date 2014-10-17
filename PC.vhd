b74f942eed646d29c319e475f4e34858523a16c0
Vu N. Quang <vungocquang92@yahoo.com>
2014-10-17 22:42:37 +0800

    complete PC.vhd
    complete PC.vhd. Still got some warning after synthesis

--- PC.vhd
+++ PC.vhd
@@ -18,6 +18,7 @@
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 entity PC is
 	Port(	PC_in 	: in STD_LOGIC_VECTOR (31 downto 0);
@@ -28,9 +29,20 @@ end PC;
 
 
 architecture arch_PC of PC is
+
 begin
+	process (CLK)
+	begin
+	--<implement PC here>
 
---<implement PC here>
+		if (CLK'event and CLK = '1') then
+			if RESET = '1' then
+				PC_out <= x"00400000";
+			end if;
+		else
+				PC_out <= PC_in + 4;
+		end if;
 
+	end process;
 end arch_PC;
 
