38e1125c676fcadab0e76417b0ca53635433d377
Vu N. Quang <vungocquang92@yahoo.com>
2014-10-29 14:40:09 +0800

    Edit logic in MIPS WriteData_reg update TOP.vhd to v2

--- TOP.vhd
+++ TOP.vhd
@@ -54,7 +54,9 @@ architecture arch_TOP of TOP is
 ----------------------------------------------------------------
 -- Constants
 ----------------------------------------------------------------
-constant CLK_DIV_BITS	: integer := 25; --25 for a clock of the order of 1Hz
+constant CLK_DIV_BITS	: integer := 26; --26 for a clock of the order of 1Hz. Changed in top.vhd_v2 : use (CLK_DIV_BITS of top.vhd_v2)+1. 
+-- 1 for a 50MHz clock.
+-- See the notes in CLK_DIV_PROCESS for SIMULATION or for obtaining a 100MHz clock frequency, 
 constant N_LEDs			: integer := 8;
 constant N_DIPs			: integer := 16;
 
@@ -70,8 +72,8 @@ component mips is
 			Data_Out			: out  STD_LOGIC_VECTOR (31 downto 0); -- Data to be written to data memory / memory-mapped peripherals 
 			MemRead 			: out STD_LOGIC; 	-- MemRead signal to data memory / memory-mapped peripherals 
 			MemWrite 		: out STD_LOGIC; 	-- MemWrite signal to data memory / memory-mapped peripherals 
-			RESET				: in STD_LOGIC; 	-- Reset signal for the processor. Should reset ALU and PC. Resetting general purpose registers is not essential (though it could be done).
-			CLK				: in STD_LOGIC 	-- Divided (lower frequnency) clock for the processor.
+			RESET				: in STD_LOGIC; 	-- Reset signal for the processor. Should reset ALU, PC and pipeline registers (if present). Resetting general purpose registers is not essential (though it could be done).
+			CLK				: in STD_LOGIC 	-- Divided (lower frequency) clock for the processor.
 			);
 end component mips;
 
@@ -90,7 +92,6 @@ signal MemWrite 		: STD_LOGIC;
 -- Others signals
 ----------------------------------------------------------------
 signal dec_DATA_MEM, dec_LED, dec_DIP : std_logic;  -- data memory address decoding
-signal DIP_debounced : STD_LOGIC_VECTOR (15 downto 0):=(others=>'0'); -- DIP switch debouncing
 signal CLK : std_logic; --divided (low freq) clock
 
 ----------------------------------------------------------------
@@ -104,22 +105,27 @@ type MEM_256x32 is array (0 to 255) of std_logic_vector (31 downto 0); -- 256 wo
 constant INSTR_MEM : MEM_256x32 := (
 			x"3c090000", -- start : lui $t1, 0x0000
 			x"35290001", -- 			ori $t1, 0x0001 # constant 1
-			x"3c081003", -- 			lui $t0, 0x1003 # DIP pointer, for VHDL
-			x"8d0c0000", --			lw  $t4, 0($t0) 
-			x"3c081002", --			lui $t0, 0x1002 # LED pointer, for VHDL
+			x"3c081002", -- 			lui $t0, 0x1002 # DIP address before offset
+			x"35088001", --			ori $t0, 0x8001
+			x"8d0c7fff", --			lw  $t4, 0x7fff($t0) # DIP address 0x10030000 = 0x10028001 + 0x7fff
+			x"3c081002", --			lui $t0, 0x1002 # LED address before offset
+			x"35080001", --			ori $t0, 0x0001
 			x"3c0a0000", -- loop: 	lui $t2, 0x0000
-			x"354a0004", -- 			ori $t2, 0x0004 # delay counter (n). Change according to the clock
+			x"354a0004", -- 			ori $t2, 0x0004 # delay counter (n) if using slow clock
+			-- x"3c0a00ff",-- 			#lui $t2, 0x00ff			
+			-- x"354affff",-- 			#ori $t2, 0xffff # delay counter (n) if using fast clock
 			x"01495022", -- delay: 	sub $t2, $t2, $t1 
 			x"0149582a", -- 			slt $t3, $t2, $t1
 			x"1160fffd", -- 			beq $t3, $zero, delay
-			x"ad0c0000", -- 			sw  $t4, 0($t0)	
+			x"ad0cffff", -- 			sw  $t4, 0xffffffff($t0)	# LED address 0x10020000 = 0x10020001 + 0xffffffff.
 			x"01806027", --			nor $t4, $t4, $zero
-			x"08100005", -- 			j loop # infinite loop; n*3 (delay instructions) + 5 (non-delay instructions).
+			x"08100007", -- 			j loop # infinite loop; n*3 (delay instructions) + 5 (non-delay instructions).
 			others=> x"00000000");
 
--- The Blinky program reads the DIP switches in the begining. Let the value read be VAL
+-- The Blinky program reads the DIP switches in the beginning. Let the value read be VAL
 -- It will then keep alternating between VAL(7 downto 0) , not(VAL(7 downto 0)), 
--- essentially blinking LED(7 downto 0) according to the initial pattern read from the DIP switches 	
+-- essentially blinking LED(7 downto 0) according to the initial pattern read from the DIP switches
+-- Changes in top.vhd_v2 : Addresses are now calculated using positive and negative offsets - to test if LW and SW works completely.
 
 ----------------------------------------------------------------
 -- Data Memory
@@ -153,7 +159,7 @@ MIPS1 : MIPS port map (
 ----------------------------------------------------------------
 -- Data memory address decoding
 ----------------------------------------------------------------
-dec_DATA_MEM <= '1' 	when Addr_Data>=x"10010000" and Addr_Data<=x"100103FC" else '0'; --assuming 256 word memory
+dec_DATA_MEM <= '1' 	when Addr_Data>=x"10010000" and Addr_Data<=x"100103FC" else '0'; -- To check if address is in the valid range, assuming 256 word memory
 dec_LED 		<= '1'	when Addr_Data=x"10020000" else '0';
 dec_DIP 		<= '1' 	when Addr_Data=x"10030000" else '0';
 
@@ -168,7 +174,7 @@ Data_In 	<= (N_DIPs-1 downto 0 => '0') & DIP							when MemRead = '1' and dec_DI
 -- Instruction memory read
 ----------------------------------------------------------------
 Instr <= INSTR_MEM(conv_integer(Addr_Instr(9 downto 2))) 
-			when ( Addr_Instr(31 downto 10) & Addr_Instr(1 downto 0) )=x"004000" -- To check if address is in the valid range. Also helps minimize warnings
+			when Addr_Instr>=x"00400000" and Addr_Instr<=x"004003FC" -- To check if address is in the valid range, assuming 256 word memory. Also helps minimize warnings (--changed in top.vhd_v2 to have a stricter check)
 			else x"00000000";
 
 ----------------------------------------------------------------
@@ -182,9 +188,11 @@ LED(15) <= CLK; -- debug showing clock
 ----------------------------------------------------------------
 write_LED: process (CLK)
 begin
-	if (CLK'event and CLK = '1') then
-		if (MemWrite = '1') and  (dec_LED = '1') then
-             LED(N_LEDs-1 downto 0) <= Data_Out(N_LEDs-1 downto 0);
+	if CLK'event and CLK = '1' then
+		if RESET = '1' then
+			LED(N_LEDs-1 downto 0) <= (others=> '0');
+		elsif (MemWrite = '1') and  (dec_LED = '1') then
+			LED(N_LEDs-1 downto 0) <= Data_Out(N_LEDs-1 downto 0);
 		end if;
 	end if;
 end process;
@@ -194,7 +202,7 @@ end process;
 ----------------------------------------------------------------
 write_DATA_MEM: process (CLK)
 begin
-    if (CLK'event and CLK = '1') then
+    if CLK'event and CLK = '1' then
         if (MemWrite = '1' and dec_DATA_MEM = '1') then
             DATA_MEM(conv_integer(Addr_Data(9 downto 2))) <= Data_Out;
         end if;
@@ -204,12 +212,15 @@ end process;
 ----------------------------------------------------------------
 -- Clock divider
 ----------------------------------------------------------------
+-- CLK <= CLK_undiv 
+-- IMPORTANT : >>> uncomment the previous line and comment out the rest of the process
+--					>>> for SIMULATION or for obtaining a 100MHz clock frequency
 CLK_DIV_PROCESS : process(CLK_undiv)
-variable clk_counter : std_logic_vector(CLK_DIV_BITS downto 0) := (others => '0');
+variable clk_counter : std_logic_vector(CLK_DIV_BITS-1 downto 0) := (others => '0');
 begin
 	if CLK_undiv'event and CLK_undiv = '1' then
 		clk_counter := clk_counter+1;
-		CLK <= clk_counter(CLK_DIV_BITS);
+		CLK <= clk_counter(CLK_DIV_BITS-1);
 	end if;
 end process;
 
@@ -226,19 +237,22 @@ end arch_TOP;
 ----------------------------------------------------------------
 -- Blinky Program
 ----------------------------------------------------------------
---ori $t1, 0x0001 # constant 1
---#lui $t0, 0x1001 # DIP pointer, for MIPS simulation
---lui $t0, 0x1003 # DIP pointer, for VHDL
---lw  $t4, 0($t0)
---lui $t0, 0x1002 # LED pointer, for VHDL
---loop:
---lui $t2, 0x0000
---ori $t2, 0x0004 # delay counter (n). Change according to the clock
---delay:
---sub $t2, $t2, $t1 
---slt $t3, $t2, $t1
---beq $t3, $zero, delay
---sw  $t4, 0($t0)
---nor $t4, $t4, $zero
---j loop
---# n*3 (delay instructions) + 5 (non-delay instructions).
+--#NOTE:	>>> for simulation in MARS, use a lower value for delay counter, 
+--#		>>> and a value closer to 0x10010000 for memory mapped devices.
+--start :lui $t1, 0x0000
+--			ori $t1, 0x0001 # constant 1
+--			lui $t0, 0x1002 # DIP address before offset
+--			ori $t0, 0x8001
+--			lw  $t4, 0x7fff($t0) # DIP address 0x10030000 = 0x10028001 + 0x7fff
+--			lui $t0, 0x1002 # LED address before offset
+--			ori $t0, 0x0001
+--loop: 	lui $t2, 0x0000
+--			ori $t2, 0x0004 # delay counter (n) if using slow clock
+--			#lui $t2, 0x00ff			
+--			#ori $t2, 0xffff # delay counter (n) if using fast clock
+--delay: sub $t2, $t2, $t1 
+--			slt $t3, $t2, $t1
+--			beq $t3, $zero, delay
+--			sw  $t4, 0xffffffff($t0)	# LED address 0x10020000 = 0x10020001 + 0xffffffff.
+--			nor $t4, $t4, $zero
+--			j loop # infinite loop; n*3 (delay instructions) + 5 (non-delay instructions).
